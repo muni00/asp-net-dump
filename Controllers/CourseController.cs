424ab9548caa498f4d3012ac31d45ed62c003a3f
Muazzez Nihal Bahadır <58309495+muni00@users.noreply.github.com>
2024-07-30 11:02:41 +0300

    test

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -1,11 +1,11 @@
 using BtkAkademi.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BtkAkademi.Controllers
+namespace BtkAkademi.Controllers 
 {
     public class CourseController : Controller
     {
-
+        //fdf
         public IActionResult Index()
         {
             var model = Repository.Applications;
@@ -20,6 +20,10 @@ namespace BtkAkademi.Controllers
         [ValidateAntiForgeryToken] // kullanılan arama motorunun güvenliğini test eder
         public IActionResult Apply([FromForm]Candidate model)
         {
+            if (Repository.Applications.Any(a => a.Email == model.Email))
+            {
+                ModelState.AddModelError("", "Tere is already application");
+            }
             if (ModelState.IsValid)
             {
                 Repository.Add(model);
